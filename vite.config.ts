8c922dd427736e93ae70d9c2cde230e37f6e7992
bugg-m <mkchauhan0543@gmail.com>
2024-11-02 00:16:05 +0530

    added daisy ui in project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port:3000,
+  },
+   resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@screens': path.resolve(__dirname, './src/screens')
+    }
+  }
 })
