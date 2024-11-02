58e067541fd58ed2b4a2ff354c1a23fdcdfa3153
bugg-m <mkchauhan0543@gmail.com>
2024-11-02 18:41:46 +0530

    prettier configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "react", "@typescript-eslint", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "prettier/prettier": ["warn", require("./.prettierrc")],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:3000,
+    port: 3000,
   },
-   resolve: {
+  resolve: {
     alias: {
-      '@src': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@screens': path.resolve(__dirname, './src/screens')
-    }
-  }
-})
+      "@src": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@screens": path.resolve(__dirname, "./src/screens"),
+    },
+  },
+});
